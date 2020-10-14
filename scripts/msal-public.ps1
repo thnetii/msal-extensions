d21edfc8cc5b5f2ccbe42b04fe6ab6193e3488c2
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-10-14 16:09:20 +0200

    MSAL for PowerShell

--- scripts/msal-public.ps1
+++ scripts/msal-public.ps1
@@ -1,10 +1,24 @@
 $EnvJson = Get-Content (Join-Path $PSScriptRoot "env.json") | ConvertFrom-Json
-$PublicJson = $EnvJson["public"]
+$PublicJson = $EnvJson.public
 
 $TfmFolder = "netcoreapp3.1"
 if ($PSVersionTable.PSEdition -ine "Core") {
     $TfmFolder = "net45"
 }
+
 Import-Module -Verbose -ErrorAction "Stop" ([System.IO.Path]::Combine($PSScriptRoot, "..", "publish", "THNETII.Msal.PowerShell", "Debug", $TfmFolder, "THNETII.Msal.PowerShell.psm1"))
 
-Get-Command New-MsalPublicClientOptions | fl *
+[Microsoft.Identity.Client.PublicClientApplicationBuilder]$Builder = New-MsalPublicClientApplicationBuilder -ClientId $PublicJson.ClientId
+$Builder.WithTenantId($PublicJson.TenantId) | Out-Null
+$Builder.WithRedirectUri("http://localhost") | Out-Null
+$MsalApp = $Builder.Build()
+$AuthFlow = $MsalApp.AcquireTokenInteractive([string[]]@())
+try {
+    $AuthResult = $AuthFlow.ExecuteAsync().GetAwaiter().GetResult()
+    $AuthResult | Format-List *
+}
+catch {
+    Write-Host $_.Exception
+}
+
+
